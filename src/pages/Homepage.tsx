13dd350c6d584859c8e40460d5ad860c015838fe
Arnold Kibira <arnold.ril3y@gmail.com>
2022-06-13 16:26:25 +0300

    set up folder structure and component skeletons

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import PostList from "../components/PostList";
+
+const Homepage = () => {
+  return (
+    <div>
+      <Navbar />
+      <PostList />
+    </div>
+  );
+};
+
+export default Homepage;
