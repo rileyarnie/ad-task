ffbd7a412df63d0d13a4962f63f70b36e30009a1
Arnold Kibira <arnold.ril3y@gmail.com>
2022-06-13 18:45:29 +0300

    Add dynamic path for post details

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import PostDetail from "./components/PostDetail";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/post/:postId" element={<PostDetail />} />
       </Routes>
     </div>
   );
