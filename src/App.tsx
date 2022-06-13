9a721401ec5ce3d3ec47fabe2155e1716d735a39
Arnold Kibira <arnold.ril3y@gmail.com>
2022-06-13 16:39:18 +0300

    set up routes and add link to roboto font for usage with material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
