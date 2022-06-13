6b7c05e55798af1766e6bd6596f46853f404da84
Arnold Kibira <arnold.ril3y@gmail.com>
2022-06-14 01:49:27 +0300

    ensure posts with their accompanying images/videos are sent and stored in firebase

--- src/components/PostModal.tsx
+++ src/components/PostModal.tsx
@@ -11,6 +11,15 @@ import FormHelperText from "@mui/material/FormHelperText";
 import Input from "@mui/material/Input";
 import InputLabel from "@mui/material/InputLabel";
 import { DropzoneArea } from "react-mui-dropzone";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { db, storage } from "../config/firebase";
+import { ref, getDownloadURL, uploadString } from "firebase/storage";
 
 interface Props {
   open: boolean;
@@ -20,6 +29,7 @@ interface Props {
 const PostModal: React.FC<Props> = (props) => {
   const [caption, setCaption] = useState("");
   const [file, setFile] = useState<any>(null);
+  const [chosenFile, setChosenFile] = useState<any>(null);
 
   const handleCaptionChange = (
     event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
@@ -27,13 +37,44 @@ const PostModal: React.FC<Props> = (props) => {
     setCaption(event.target.value);
   };
 
-  const handleChange = (files: any) => {
-    setFile(files[0]);
+  const handleFileChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    const target = event.target as HTMLInputElement;
+    if (!target.files) {
+      return;
+    }
+    setChosenFile(target.files[0]);
+
+    const fileReader = new FileReader();
+    fileReader?.readAsDataURL(target.files[0]);
+    fileReader.onload = (event) => {
+      setFile(event.target?.result);
+    };
   };
 
-  const handleSubmit = () => {
-    const postData = { caption, fileName: file.name, fileType: file.type };
-    console.log(postData);
+  const handleSubmit = async () => {
+    //create poat in firebase
+    //get post id
+    //upload image/video
+    // get download url for image/video
+    const documentRef = await addDoc(collection(db, "posts"), {
+      username: "Batman",
+      caption,
+      fileType: chosenFile.type,
+      timestamp: serverTimestamp(),
+    });
+
+    const fileRef = ref(storage, `posts/${documentRef.id}/image`);
+
+    await uploadString(fileRef, file, "data_url")
+      .then(async (snapshot) => {
+        const downloadURL = await getDownloadURL(fileRef);
+        await updateDoc(doc(db, "posts", documentRef.id), {
+          fileUrl: downloadURL,
+        });
+      })
+      .catch((err) => console.log({ err }));
   };
 
   return (
@@ -56,26 +97,29 @@ const PostModal: React.FC<Props> = (props) => {
             name="file"
             required
           />
-          {/* <Input
+          <input
             id="my-input"
             aria-describedby="my-helper-text"
             placeholder=""
             type="file"
             onChange={(event) => handleFileChange(event)}
-          /> */}
-          <DropzoneArea
+            // accept={("image/jpeg", "image/png", "image/bmp", "video/mp4")}
+            accept="image/*, video/mp4"
+          />
+          {/* <DropzoneArea
             dropzoneText={"Drop a file or clck"}
             filesLimit={1}
             maxFileSize={5000000}
             useChipsForPreview
-            onChange={handleChange}
+            // onChange={(files, event) => handleChange(files, event)}
+            onDrop={(files, event) => handleChange(files, event)}
             acceptedFiles={[
               "image/jpeg",
               "image/png",
               "image/bmp",
               "video/mp4",
             ]}
-          />
+          /> */}
         </FormControl>
       </DialogContent>
       <DialogActions>
